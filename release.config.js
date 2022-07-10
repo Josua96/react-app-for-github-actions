7dda67c0d75f12e52f0c4f29cc5ef27ffbc748ea
Josua Enrique Carranza Pérez <joshuaenrique96@gmail.com>
2022-07-09 20:32:14 -0600

    Develop (#6)
    * Adding develop workflow for  pull requests (#1)
    
    * Adding develop workflow for  pull requests
    
    * fixing workflow name
    
    * renaming github folder
    
    * recreating the github actions folder
    
    * applying prettier
    
    * Adding push flow for the develop branch
    
    * applying pretiter to the ci.yml file
    
    * Caching dependencies (#3)
    
    * caching dependencies
    
    * remvoing comment
    
    * applying prettier
    
    * Uploading artifacts (#4)
    
    * uploading artifacts
    
    * adding condition for build
    
    * Creating release step 1 (#5)
    
    * fix: adding github release
    
    * feat: new feature
    
    * running semantic release

---
+++ release.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  branches: "master",
+  repositoryUrl: "https://github.com/Josua96/react-app-for-github-actions",
+  plugins: [
+    "@semantic-release/commit-analyzer", // will analyze commits messages and will determine which is the next version for the release.
+    "@semantic-release/release-notes-generator", // will generate release notes based on the commit messages
+    "@semantic-release/npm", // useful if you are releasing an npm package
+    "@semantic-release/github", // in charge of creating the github release, the one that you can see on the releases section of the github repository
+  ],
+};
