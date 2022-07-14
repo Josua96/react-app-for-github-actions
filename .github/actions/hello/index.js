1440216b1140255361daf681f6001f4d99bb9ee9
Josua96 <joshuaenrique96@gmail.com>
2022-07-13 19:35:12 -0600

    feat(github core features): github core features

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -4,10 +4,21 @@ const github = require("@actions/github");
 try {
   const name = core.getInput("who-to-greet");
 
+  core.debug("Debug message");
+  core.warning("Warning message");
+  core.error("Error message");
+
+  // the value of the name variable will not appear in logs since this line mark it as secret
+  core.setSecret(name);
+
+  core.startGroup("Logging github object");
   console.log(`Hello ${name}`);
+  core.endGroup();
 
   const time = new Date();
   core.setOutput("time", time.toTimeString());
+
+  core.exportVariable("HELLO", "hello");
 
   console.log(JSON.stringify(github, null, "\t"));
 } catch (error) {
