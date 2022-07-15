a498f2fa35c5e377eb25819553634d1d8bb9402a
Josua96 <joshuaenrique96@gmail.com>
2022-07-14 19:41:27 -0600

    feat(adding php script): php script

---
+++ .github/actions/slack/script.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once 'vendor/autoload.php';
+Requests::register_autoloader();
+
+var_dump($argv);
+var_dump($_ENV);
+
+// $response = Requests::post(
+//     "https://hooks.slack.com/services/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",
+//     array(
+//         'Content-type' => 'application/json'
+//     ),
+//     json_encode(array (
+//         'blocks' => 
+//             array (
+//                 array (
+//                     "type" => "section",
+//                     "text" => array (
+//                         "type" => "mrkdwn",
+//                         "text" => $_ENV['INPUT_MESSAGE'],
+//                     ),
+//                 ),
+//                 array (
+//                     "type" => "section",
+//                     "fields" => array (
+//                         array (
+//                             "type" => "mrkdwn",
+//                             "text" => "*Repository:*\n{$_ENV['GITHUB_REPOSITORY']}",
+//                         ),
+//                         array (
+//                             "type" => "mrkdwn",
+//                             "text" => "*Event:*\n{$_ENV['GITHUB_EVENT_NAME']}",
+//                         ),
+//                         array (
+//                             "type" => "mrkdwn",
+//                             "text" => "*Ref:*\n{$_ENV['GITHUB_REF']}",
+//                         ),
+//                         array (
+//                             "type" => "mrkdwn",
+//                             "text" => "*SHA:*\n{$_ENV['GITHUB_SHA']}",
+//                         ),
+//                     ),
+//                 ),
+//             ),
+//     ))
+//     );
+
+
+// var_dump($response);
+
+// if (!$response->success) {
+//     echo $response->body
+// }
